2dab90b6896230d742b313d2324514e28ca0196b
Matt Thyng <mattthyng@gmail.com>
2014-01-20 19:29:14 -0500

    Initial  NetBeans Project  for Widgets

---
+++ Dashboard14Widgets/src/team1073/smartdashboard/extensions/battery/BatteryWidget.java
@@ -0,0 +1,15 @@
+package team1073.smartdashboard.extensions.battery;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author team1073
+ */
+public class BatteryWidget {
+    
+}
