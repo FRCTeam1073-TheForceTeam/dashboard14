aecac68be1bfbadf56b846f458ac90ec8e52c294
wmfish <wmfish@watrousweb.com>
2014-01-25 11:53:12 -0500

    Fixed Control Modes Widget

--- Dashboard14Widgets/src/team1073/smartdashboard/extensions/controlmodes/ControlModesWidget.java
+++ Dashboard14Widgets/src/team1073/smartdashboard/extensions/controlmodes/ControlModesWidget.java
@@ -7,24 +7,32 @@
 package team1073.smartdashboard.extensions.controlmodes;
 
 
+import edu.wpi.first.smartdashboard.gui.StaticWidget;
 import edu.wpi.first.smartdashboard.gui.Widget;
+import edu.wpi.first.smartdashboard.properties.MultiProperty;
 import edu.wpi.first.smartdashboard.properties.Property;
 import edu.wpi.first.smartdashboard.types.DataType;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 
-public class ControlModesWidget extends Widget
+public class ControlModesWidget extends StaticWidget
 {
     public static final DataType[] TYPES = {DataType.NUMBER };
     public static final String NAME = "Control Mode";
     private int value = -1;
+    public final MultiProperty controlModes = new MultiProperty(this, "Control Mode");
 
-    @Override
+    //@Override
+    public ControlModesWidget(){
+        controlModes.add("Off", -1);
+        controlModes.add("Autonomous", 0);
+        controlModes.add("TeleOp", 1);
+    }
     public void setValue(Object o) 
     {
-        this.value = ((Number) value).intValue();
-        
+        this.value = ((Number) o).intValue();
+       
         repaint();
 
     }
@@ -32,13 +40,14 @@ public class ControlModesWidget extends Widget
     @Override
     public void init() 
     {
-        setPreferredSize(new Dimension(50, 127));
+        setPreferredSize(new Dimension(127, 50));
     }
 
     @Override
     public void propertyChanged(Property prprt) 
     {
-   
+        setValue(prprt.getValue());
+        //repaint();
     }
     
     @Override
@@ -55,18 +64,24 @@ public class ControlModesWidget extends Widget
         {
             mode = "Off";
             g.setColor(Color.RED);
+            g.fillRect(0, 0, size.width, size.height);
+            g.setColor(Color.black);
             g.drawString(mode, size.width/5, size.height/5);
         }
         else if (value == 0)
         {
             mode = "Autonomous";
             g.setColor(Color.orange);
+            g.fillRect(0, 0, size.width, size.height);
+            g.setColor(Color.black);
             g.drawString(mode, size.width/5, size.height/5);
         }
         else
         {
          mode = "Teleoperated";
          g.setColor(Color.GREEN);
+         g.fillRect(0, 0, size.width, size.height);
+         g.setColor(Color.black);
          g.drawString(mode, size.width/5, size.height/5);
         }
         
