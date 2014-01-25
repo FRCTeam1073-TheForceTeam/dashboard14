59ee220284fe107723ba3606008189699938e459
wmfish <wmfish@watrousweb.com>
2014-01-25 15:11:30 -0500

    PSI Gauge Diags

--- Dashboard14Widgets/src/team1073/smartdashboard/extensions/psigauge/PSIGauge.java
+++ Dashboard14Widgets/src/team1073/smartdashboard/extensions/psigauge/PSIGauge.java
@@ -7,21 +7,27 @@
 package team1073.smartdashboard.extensions.psigauge;
 
 import edu.wpi.first.smartdashboard.gui.Widget;
-//import edu.wpi.first.smartdashboard.properties.MultiProperty;
+import edu.wpi.first.smartdashboard.properties.MultiProperty;
 import edu.wpi.first.smartdashboard.properties.Property;
 import edu.wpi.first.smartdashboard.types.DataType;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import javax.swing.JTextField;
 
 public class PSIGauge extends Widget 
 {
     public static final DataType[] TYPES = {DataType.NUMBER };
     public static final String NAME = "PSI Gauge";
     private double value = 0.0;
-        private JTextField jTextField2;
+    public final MultiProperty PSI = new MultiProperty(this, "PSI");
 
+    public PSIGauge(){
+        /*This constructor is only necessary for testing purposes*/
+        PSI.add("2 Shots", 60.0);
+        PSI.add("1 Shot", 31.0);
+        PSI.add("No Shots", 10.0);
+
+    }
     
     @Override
     public void setValue(Object o) 
@@ -35,7 +41,7 @@ public class PSIGauge extends Widget
     @Override
     public void init() 
     {
-        setPreferredSize(new Dimension(60, 20));
+        setPreferredSize(new Dimension(200, 90));
     }
 
     @Override
@@ -52,23 +58,26 @@ public class PSIGauge extends Widget
         g.setColor(Color.black);
         g.fillRect(0, 0, size.width, size.height);
                 
-        if (value == 60)
+        if (value == 60.0)
         {
             g.setColor(Color.GREEN);
-            g.fillRect(0, 0, size.width, size.height);
-            jTextField2.setText("Two Shots Ready");
+            g.fillRect(0, size.height - (int) (value), size.width,size.height);
+            g.setColor(Color.WHITE);
+            g.drawString("Two Shots Ready", size.width /3, size.height / 5);
         }
-        else if (value <= 60 && value > 30)
+        else if (value < 60.0 && value > 30.0)
         {
             g.setColor(Color.YELLOW);
-            g.fillRect(0, 0, size.width, (int) (size.height - value/3.5));
-            jTextField2.setText("One Shot Ready");
+            g.fillRect(0, size.height - (int) (value), size.width,size.height);
+            g.setColor(Color.WHITE);
+            g.drawString("One Shot Ready", size.width /3, size.height / 5);
         }
-        else if (value <= 30)
+        else if (value <= 30.0)
         {
               g.setColor(Color.RED);
-              g.fillRect(0, 0, size.width, (int) (size.height - value*2.5));
-              jTextField2.setText("Zero Shots Ready");
+              g.fillRect(0, size.height - (int) (value), size.width,size.height);
+              g.setColor(Color.WHITE);
+              g.drawString("Not Ready", size.width /3, size.height / 5);
         }
        
     }
