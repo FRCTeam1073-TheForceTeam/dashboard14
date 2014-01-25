c4115f7a06dcd24ce36510312eadae00efd7847e
wmfish <wmfish@watrousweb.com>
2014-01-25 11:58:01 -0500

    Made Static Widget nonstatic

--- Dashboard14Widgets/src/team1073/smartdashboard/extensions/controlmodes/ControlModesWidget.java
+++ Dashboard14Widgets/src/team1073/smartdashboard/extensions/controlmodes/ControlModesWidget.java
@@ -7,7 +7,6 @@
 package team1073.smartdashboard.extensions.controlmodes;
 
 
-import edu.wpi.first.smartdashboard.gui.StaticWidget;
 import edu.wpi.first.smartdashboard.gui.Widget;
 import edu.wpi.first.smartdashboard.properties.MultiProperty;
 import edu.wpi.first.smartdashboard.properties.Property;
@@ -16,19 +15,20 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 
-public class ControlModesWidget extends StaticWidget
+public class ControlModesWidget extends Widget
 {
     public static final DataType[] TYPES = {DataType.NUMBER };
     public static final String NAME = "Control Mode";
     private int value = -1;
     public final MultiProperty controlModes = new MultiProperty(this, "Control Mode");
 
-    //@Override
+    
     public ControlModesWidget(){
         controlModes.add("Off", -1);
         controlModes.add("Autonomous", 0);
         controlModes.add("TeleOp", 1);
     }
+    @Override
     public void setValue(Object o) 
     {
         this.value = ((Number) o).intValue();
